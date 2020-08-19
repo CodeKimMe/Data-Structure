35aa34b3cd93c27a7fb1cd5e59ffdedbfae43633
CodeKimeMe <kkt3165@naver.com>
2020-08-19 21:46:07 +0900

    first

---
+++ Main.java
@@ -0,0 +1,22 @@
+package exercise;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		Solution Sol = new Solution();
+		char[][] gridA = {{'1','0','0'},{'0','0','1'},{'0','1','1'}};
+		int ans = Sol.numIslands(gridA);
+		System.out.println("number of islands is " + ans);
+		*/
+		DynamicArray A = new DynamicArray();
+		A.add(5);
+		A.add(7);
+		A.add(10);
+		A.add(13);
+		A.add(22);
+		A.add(4);
+		A.printArray();
+	}
+
+}
